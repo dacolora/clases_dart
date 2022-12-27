9f47c36d892aab7e5d52d785cfcf23cc94b95636
Daniel Colorado <dacolora@bancolombia.com.co>
2022-12-27 15:38:25 -0500

    add background

--- first_sprite/lib/main.dart
+++ first_sprite/lib/main.dart
@@ -1,17 +1,37 @@
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(GameWidget(game: Game()));
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.setLandscape();
 }
 
 class Game extends FlameGame {
   SpriteComponent king = SpriteComponent();
   SpriteComponent man = SpriteComponent();
+  SpriteComponent background = SpriteComponent();
+  double sizeComponent = 150.0;
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    print('ejecicion del juego');
+    background
+      ..sprite = await loadSprite('background.jpg')
+      ..size = size;
+
+    king
+      ..sprite = await loadSprite('king.png')
+      ..size = Vector2(sizeComponent, sizeComponent)
+      ..position = Vector2(20, size.y - sizeComponent);
+    man
+      ..sprite = await loadSprite('man.png')
+      ..size = Vector2(sizeComponent, sizeComponent)
+      ..position = Vector2(250, size.y - sizeComponent);
+    add(background);
+    add(king);
+    add(man);
   }
 }
